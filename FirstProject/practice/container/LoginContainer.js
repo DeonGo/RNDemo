ab0484096ed3286c8c34924224595ef0b24dbb21
dgao <dgao@xogrp.com>
2018-03-01 16:58:41 +0800

    add custom button instead of button

--- FirstProject/practice/container/LoginContainer.js
+++ FirstProject/practice/container/LoginContainer.js
@@ -4,6 +4,7 @@ import { View, Text, TextInput, Button, Alert } from 'react-native';
 import * as loginInteractor from '../interactor/LoginInteractor'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
+import { StyleSheet,TouchableOpacity } from 'react-native';
 
 
 
@@ -49,7 +50,7 @@ class LoginContainer extends React.Component {
                     (text) => this.setState({ pwd: text })
                 } />
 
-                <Button title="Login" onPress={() => login(this.state.username, this.state.pwd)} />
+                <ImageButton text='login' onLoginClicked={() => login(this.state.username, this.state.pwd)} />
                 <Button title="Signup" onPress={() => navigate('Signup', { name: 'Signup' })} />
             </View>
         )
@@ -60,10 +61,27 @@ class LoginContainer extends React.Component {
         this.props.clearState();
     };
 
+    
+
     // login() {
     //     alert("username:" + this.state.username + "  pwd:" + this.state.pwd);
     // }
 };
+
+
+export class ImageButton extends React.Component{
+    render(){
+      return(
+        <TouchableOpacity onPress={this.props.onLoginClicked} activeOpacity={0.2} focusedOpacity={0.5}>
+           <View style=  {{justifyContent:'center',alignItems:'center',width:128,height:40,backgroundColor:'#841584'}}>
+            {/* <Image source={require('./image/ic_search.png')}> */}
+            {/* </Image> */}
+            <Text style={{color:'#ffffff'}}>{this.props.text}</Text>
+           </View>
+        </TouchableOpacity>
+    );
+    }
+  }
 
 
 
