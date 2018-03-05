990c1305b59a3dbc4057287262619335569a064a
dgao <dgao@xogrp.com>
2018-03-05 16:03:34 +0800

    add md lib

--- FirstProject/practice/container/LoginContainer.js
+++ FirstProject/practice/container/LoginContainer.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { View, Text, TextInput, Alert } from 'react-native';
+
+import { Button } from 'react-native-material-design';
 
 import * as loginInteractor from '../interactor/LoginInteractor'
 import { connect } from 'react-redux';
@@ -51,7 +53,7 @@ class LoginContainer extends React.Component {
                 } />
 
                 <ImageButton text='login' onLoginClicked={() => login(this.state.username, this.state.pwd)} />
-                <Button title="Signup" onPress={() => navigate('Signup', { name: 'Signup' })} />
+                <Button value="NORMAL FLAT" raised={true} text="Signup" onPress={() => navigate('Signup', { name: 'Signup' })} />
             </View>
         )
     }
@@ -73,7 +75,7 @@ export class ImageButton extends React.Component{
     render(){
       return(
         <TouchableOpacity onPress={this.props.onLoginClicked} activeOpacity={0.2} focusedOpacity={0.5}>
-           <View style=  {{justifyContent:'center',alignItems:'center',width:128,height:40,backgroundColor:'#841584'}}>
+           <View style=  {{alignItems:'center',justifyContent:'center',alignItems:'center',width:128,height:40,backgroundColor:'#841584'}}>
             {/* <Image source={require('./image/ic_search.png')}> */}
             {/* </Image> */}
             <Text style={{color:'#ffffff'}}>{this.props.text}</Text>
